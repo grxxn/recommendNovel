08ebf11269aaa3320d118423a37d381c7ca5e6b6
grxxn <jcb8965@naver.com>
2022-07-14 10:54:55 +0900

    modify recommend card style

--- src/components/Recommend.js
+++ src/components/Recommend.js
@@ -1,10 +1,20 @@
 import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
 const Recommend = ({id, books}) => {
+  const navigate = useNavigate();
+
   const recBookList = books.map((item, idx)=>{
     return(
     idx !== parseInt(id)
-    ? <span className='book-content'>
+    ? <span 
+        className='book-content' 
+        key={item.id}
+        onClick={()=>{
+          navigate(`/recommendNovel/detail/${item.id}`);
+          window.scrollTo(0, 0)
+        }}
+      > 
         <img src={require('../img/book' + item.id + '.jpg')} alt={item.title}/>
         <span>{item.title}</span>
       </span>
